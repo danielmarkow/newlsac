b85e648be4a010d93ff80aebc6e37e6657f1b657
Daniel Markow <d_markow@icloud.com>
2023-11-22 11:28:59 +0100

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
