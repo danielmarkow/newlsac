806d87d0669733047bb41159d8954556a502f4a2
Daniel Markow <d_markow@icloud.com>
2023-11-22 11:20:00 +0100

    fixed error in col naming

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -4,12 +4,12 @@ import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 export const links = pgTable(
 	'links',
 	{
-		id: varchar('offer_id', { length: 21 }).primaryKey(),
+		id: varchar('link_id', { length: 21 }).primaryKey(),
 		userEmail: varchar('user_id', { length: 320 }).notNull(),
 		linkUrl: text('link_url').notNull(),
 		comment: text('comment'),
 		createdAt: timestamp('created_at').defaultNow().notNull(),
-		updatedAt: timestamp('updatedAt').notNull()
+		updatedAt: timestamp('updated_at').notNull()
 	},
 	(links) => ({
 		userEmailIndex: index('user_email_idx').on(links.userEmail)
